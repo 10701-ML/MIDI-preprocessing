873a118b3bceff74d0c464f37f75bf7e57af02a0
adam <adam1679@163.com>
2019-12-02 11:34:12 -0500

    update

--- src/parameters.py
+++ src/parameters.py
@@ -3,6 +3,8 @@ hidden_dim = 256
 input_dim = 88
 output_dim = 88
 path = "../data/chp_op18.mid"
+batch_size = 32
+learning_rate = 1e-3
 CONFIG = {
     'beat_resolution': 24, # temporal resolution (in time step per beat)
     'time_signatures': ['4/4'], # '3/4', '2/4'
--- src/train_left_right.py
+++ src/train_left_right.py
@@ -15,56 +15,72 @@ import argparse
 import json
 
 device = torch.device("cpu")
+from seq2seq_model import DecoderRNN, EncoderRNN
+from midi_io_dic_mode import *
+from parameters import *
+import torch
+from torch import optim
+from torch import nn
+import time
+import math
+import argparse
 
-def asMinutes(s):
-    m = math.floor(s / 60)
-    s -= m * 60
-    return '%dm %ds' % (m, s)
+device = torch.device("cpu")
+
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer,
+          decoder_optimizer, criterion):
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    target_length = target_tensor.size(0)
+    encoder_output, encoder_hidden = encoder(input_tensor)  # (time_len, batch_size)
+    decoder_input = torch.zeros((1, target_tensor.size(1), target_tensor.size(2)), dtype=torch.float, device=device)
+    decoder_hidden = encoder_hidden
+    ones = torch.ones(input_tensor.size(1), input_tensor.size(2))
+    zeros = torch.zeros(input_tensor.size(1), input_tensor.size(2))
+
+    loss = 0
+
+    # Teacher forcing: Feed the target as the next input
+    for di in range(target_length):
+        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden) # decoder_output：(1, B, D)
+        loss += criterion(decoder_output[0], target_tensor[di])
+
+        if torch.rand(1)[0] > threshold:
+            decoder_input = target_tensor[di].unsqueeze(0)
+        else:
+            decoder_input = torch.where(decoder_output[0, :, :] > 0.5, ones, zeros)
+            decoder_input = decoder_input.unsqueeze(0).detach()  # detach from history as input
+
+    loss.backward()
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
 
 
-def timeSince(since, percent):
-    now = time.time()
-    s = now - since
-    es = s / (percent)
-    rs = es - s
-    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
-
-
-def trainIters(train_x, train_y, model, token_size, learning_rate=1e-3, batch_size=32):
-    model.train()
+def trainIters(train_x, train_y, encoder, decoder, learning_rate=1e-3, batch_size=32):
     print_loss_total = 0  # Reset every print_every
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    criterion = nn.CrossEntropyLoss()
-    training_x = torch.tensor(train_x, dtype=torch.long) # (num_of_songs, num_of_samples, time_len)
-    training_y = torch.tensor(train_y, dtype=torch.long)
+
+    encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
+    training_x = torch.tensor(train_x, dtype=torch.float)
+    training_y = torch.tensor(train_y, dtype=torch.float)
+    criterion = nn.BCELoss(reduction="sum")
     for iter in range(1, training_x.size(0)+1): # iterate each sone
-        optimizer.zero_grad()
         input_tensor = training_x[iter-1]
         target_tensor = training_y[iter-1]
         loss = 0
         for i in range(0, input_tensor.size(1), batch_size):
-            input_tensor_batch = input_tensor[:, i: i+batch_size]   #(time_len, batch_size)
-            target_tensor_batch = target_tensor[:, i: i+batch_size]  #(time_len, batch_size)
-            target_size = input_tensor_batch.size(0)
-            input = torch.zeros_like(input_tensor_batch[0:1, :])
-            hidden = None
-            for di in range(target_size):
-                output, hidden = model(input, hidden)
-                target = target_tensor_batch[di, :]
-                output = output.reshape(-1, token_size)
-                loss += criterion(output, target)
-                if torch.rand(1)[0] > threshold:
-                    input = target_tensor_batch[di].unsqueeze(0)
+            loss += train(input_tensor[:, i: i+batch_size], target_tensor[:, i: i+batch_size],
+                          encoder, decoder, encoder_optimizer, decoder_optimizer, criterion)
 
-                else:
-                    input = torch.argmax(output, dim=1)
-                    input = input.unsqueeze(0).detach()
-
-        loss.backward()
-        optimizer.step()
         print_loss_total += loss
 
     return print_loss_total
+
+
+
 
 def train_left(x, y, y_token_size=None, x_token_size=None, embed_size=None):
     # one_hot = lambda x, token_size: np.eye(token_size)[x]
@@ -117,22 +133,26 @@ if __name__ == "__main__":
     # step 2 : generated the right-hand music
     pianoroll_data = midiToPianoroll(midi_path, merge=False, velocity=False) # (n_time_stamp, 128, num_track)
     right_track, left_track = pianoroll_data[:, :, 0], pianoroll_data[:, :, 1]
-    input_datax, input_datay = createSeqNetInputs([right_track], time_len , 1, right_corpus)
-    model = Sequence(right_token_size, emb_size, hidden_dim)
+    input_datax, input_datay = createSeqNetInputs([right_track], time_len , 1)
+
+    encoder1 = EncoderRNN(input_dim, hidden_dim).to(device)
+    decoder1 = DecoderRNN(input_dim, hidden_dim).to(device)
     if args.load_epoch != 0:
-        model.load_state_dict(torch.load(f'../models/{model_name}_' + str(args.load_epoch) + '_Adam1e-3'))
+        encoder1.load_state_dict(torch.load('../models/encoder_baseline_' + str(args.load_epoch) + '_Adam1e-3'))
+        decoder1.load_state_dict(torch.load('../models/decoder_baseline_' + str(args.load_epoch) + '_Adam1e-3'))
 
     print("shape of data ", pianoroll_data.shape)
     for i in range(1, args.epoch_number+1):
-        loss = trainIters(input_datax, input_datay, model, token_size=right_token_size)
+        loss = trainIters(input_datax, input_datay, encoder1, decoder1)
         print(f'{i} loss {loss}')
         if i % 10 == 0:
-            torch.save(model.state_dict(), f'../models/{model_name}_' + str(i + args.load_epoch) + '_Adam1e-3')
+            torch.save(encoder1.state_dict(), '../models/encoder_baseline_' + str(i + args.load_epoch) + '_Adam1e-3')
+            torch.save(decoder1.state_dict(), '../models/decoder_baseline_' + str(i + args.load_epoch) + '_Adam1e-3')
 
-    right_track_input_of_gen = pianoroll2dicMode(right_track, right_corpus)
-    input_datax = torch.tensor(right_track_input_of_gen[:origin_length], dtype=torch.long).unsqueeze(1)
+    # generating
+    input_datax = torch.tensor(right_track[:origin_length], dtype=torch.long).unsqueeze(1)
 
-    output, generate_seq = generate(input_datax, model, target_length)
+    output, generate_seq = generate(input_datax, encoder1, decoder1, target_length)
     output = [x.item() for x in output]  # real input + generated output
     generate_seq = [x.item() for x in generate_seq]  # generated output
 
